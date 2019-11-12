cfda4251b10a2d69fd8046af8d10dbb31dff4510
Nikolaj Dufke <nikolajdufke@live.dk>
2019-11-12 09:44:30 +0100

    Created workplanner project Created WorkplannerDB project Added WorkplannerDB to WorkplannerDB project

---
+++ WorkPlannerSolution/WorkPlannerDB/dbo/Tables/Employee.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]  INT          NOT NULL,
+    [FirstName]   VARCHAR (50) NOT NULL,
+    [LastName]    VARCHAR (50) NOT NULL,
+    [PhoneNumber] INT          NOT NULL,
+    [Email]       VARCHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC)
+);
+
---
+++ WorkPlannerSolution/WorkPlannerDB/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User] (
+    [UserID]     INT           NOT NULL,
+    [EmployeeID] INT           NOT NULL,
+    [Password]   NVARCHAR (50) NOT NULL,
+    [Access]     INT           NOT NULL,
+    PRIMARY KEY CLUSTERED ([UserID] ASC)
+);
+
