e908f81a8138841f12947d2dc1a7b3793e75afd2
Daniel Octavius Palmqvist <dani086m@edu.easj.dk>
2019-11-18 16:19:29 +0100

    DB update + Add EmployeeInformation table Update script to CHECKIDENT

--- WorkPlannerSolution/WorkPlannerDB/DataImports/EmployeeImportScript.sql
+++ WorkPlannerSolution/WorkPlannerDB/DataImports/EmployeeImportScript.sql
@@ -1,9 +1,29 @@
-﻿INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bob', 'Seequel', 23232323, 'bob@bobs.dk',1)
-INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bobbi', 'Seequel', 24242424, 'bobbi@bobs.dk',2)
-INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bobsa', 'Seequel', 25252525, 'bobsa@bobs.dk',3)
-INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bobsai', 'Seequel', 26262626, 'bobsai@bobs.dk',4)
-INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bobla', 'Seequel', 27272727, 'bobla@bobs.dk',5)
-INSERT INTO Employee(FirstName,LastName,PhoneNumber,Email,UserID) VALUES ('Bobil', 'Seequel', 28282828, 'bobil@bobs.dk',6)
+﻿DELETE from Employee
+DELETE from EmployeeInformation
+DELETE from Users
+DELETE from Access
+
+
+--TRUNCATE table Employee
+--TRUNCATE table EmployeeInformation
+--TRUNCATE table Users
+--TRUNCATE table Access
+
+DBCC CHECKIDENT ('Users', RESEED,0)
+DBCC CHECKIDENT ('Employee', RESEED,0)
+DBCC CHECKIDENT ('EmployeeInformation', RESEED,0)
+
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Benjamin', 'Sørensen', 23232323, 'Benjamin@WorkPlanner.dk', 'Avej', 'Aby', 1234)
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Nikolaj', 'Dufke', 24242424, 'Nikolaj@WorkPlanner.dk', 'Bvej', 'Bby', 2345)
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Sarah', 'Waest', 25252525, 'Sarah@WorkPlanner.dk', 'Bvej', 'Bby', 2345)
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Daniel', 'Palmqvist', 26262626, 'Daniel@WorkPlanner.dk', 'Cvej', 'Cby', 3456)
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Mohammed', 'El-Ali', 27272727, 'Mohammed@WorkPlanner.dk', 'Dvej', 'Dby', 4567)
+INSERT INTO EmployeeInformation(FirstName,LastName,PhoneNumber,Email,Adress,City,ZipPostal) VALUES ('Charlotte', 'Something', 28282828, 'Charlotte@WorkPlanner.dk', 'Evej', 'Eby', 5678)
+
+INSERT INTO Access(AccessLevel,AccessDescription) VALUES (1, 'Guest')
+INSERT INTO Access(AccessLevel,AccessDescription) VALUES (2, 'User')
+INSERT INTO Access(AccessLevel,AccessDescription) VALUES (3, 'Admin')
+INSERT INTO Access(AccessLevel,AccessDescription) VALUES (4, 'SuperAdmin')
 
 INSERT INTO Users(UserPassword,AccessLevel) VALUES ('kode1',1)
 INSERT INTO Users(UserPassword,AccessLevel) VALUES ('kode2',1)
@@ -12,7 +32,9 @@ INSERT INTO Users(UserPassword,AccessLevel) VALUES ('kode4',2)
 INSERT INTO Users(UserPassword,AccessLevel) VALUES ('kode5',3)
 INSERT INTO Users(UserPassword,AccessLevel) VALUES ('kode6',1)
 
-INSERT INTO Access(AccessLevel,AccessDescription) VALUES (0, 'Guest')
-INSERT INTO Access(AccessLevel,AccessDescription) VALUES (1, 'User')
-INSERT INTO Access(AccessLevel,AccessDescription) VALUES (2, 'Admin')
-INSERT INTO Access(AccessLevel,AccessDescription) VALUES (3, 'SuperAdmin')
+INSERT INTO Employee(UserID,EInformationID) VALUES (1,1)
+INSERT INTO Employee(UserID,EInformationID) VALUES (2,2)
+INSERT INTO Employee(UserID,EInformationID) VALUES (3,3)
+INSERT INTO Employee(UserID,EInformationID) VALUES (4,4)
+INSERT INTO Employee(UserID,EInformationID) VALUES (5,5)
+INSERT INTO Employee(UserID,EInformationID) VALUES (6,6)
--- WorkPlannerSolution/WorkPlannerDB/dbo/Tables/Employee.sql
+++ WorkPlannerSolution/WorkPlannerDB/dbo/Tables/Employee.sql
@@ -1,10 +1,8 @@
 ﻿CREATE TABLE [dbo].[Employee] (
     [EmployeeId]  INT          NOT NULL IDENTITY ,
-    [FirstName]   VARCHAR (50) NOT NULL,
-    [LastName]    VARCHAR (50) NOT NULL,
-    [PhoneNumber] INT          NOT NULL,
-    [Email]       VARCHAR (50) NOT NULL,
     [UserID] INT NOT NULL, 
+    [EInformationID] INT NOT NULL, 
     CONSTRAINT [PK_Employee] PRIMARY KEY ([EmployeeId]), 
     CONSTRAINT [FK_Employee_Users] FOREIGN KEY ([UserID]) REFERENCES [Users]([UserID]), 
+    CONSTRAINT [FK_Employee_EmployeeInformation] FOREIGN KEY ([EInformationID]) REFERENCES [EmployeeInformation]([EInformationID]), 
     );
