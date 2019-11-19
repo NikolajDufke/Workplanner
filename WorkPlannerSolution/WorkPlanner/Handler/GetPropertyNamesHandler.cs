72271332e981e2ec6b727c7ab2d70a3d86036126
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2019-11-19 09:56:37 +0100

    Some changes to the views
    started on getting the property names from a handler

---
+++ WorkPlannerSolution/WorkPlanner/Handler/GetPropertyNamesHandler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkPlanner.Handler
+{
+    class GetPropertyNamesHandler<T>
+    {
+        public GetPropertyNamesHandler(T obj)
+        {
+            var Props = obj.GetType().GetProperties();
+        }
+    }
+}
--- WorkPlannerSolution/WorkPlanner/ViewModel/MainPageViewModel.cs
+++ WorkPlannerSolution/WorkPlanner/ViewModel/MainPageViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WorkPlanner.VievModel
+namespace WorkPlanner.ViewModel
 {
     class MainPageViewModel
     {
