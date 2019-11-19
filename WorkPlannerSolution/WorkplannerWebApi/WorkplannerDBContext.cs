223e63a1f9ca717393f39436dde2643504c9f73e
Sarah Katrine Minjon Waest <sara693f@edu.easj.dk>
2019-11-19 14:43:50 +0100

    Changed ConnectionString

--- WorkPlannerSolution/WorkplannerWebApi/WorkplannerDBContext.cs
+++ WorkPlannerSolution/WorkplannerWebApi/WorkplannerDBContext.cs
@@ -27,7 +27,7 @@ namespace WorkplannerWebApi
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("C:\\Users\\West\\Source\\Repos\\NikolajDufke\\Workplanner\\WorkPlannerSolution\\WorkPlannerDB\\bin\\Debug\\WorkPlannerDB.dacpac");
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=WorkPlannerDB;Integrated Security=True");
             }
         }
 
