b4fb8d24c7bb841c1ec846a7988145ce6c0225a2
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2019-12-03 14:15:33 +0100

    Added a handler to login changes to login page changes to login viewmodel

--- WorkPlannerSolution/WorkPlanner/Model/TimeIntervalDetails.cs
+++ WorkPlannerSolution/WorkPlanner/Model/TimeIntervalDetails.cs
@@ -16,19 +16,19 @@ namespace WorkPlanner.Model
             _eventMembers = new Dictionary<int, Employees>();
         }
 
-        public string GetMembers
-        {
-            get
-            {
-                StringBuilder sb = new StringBuilder();
-                foreach (var member in _eventMembers)
-                {
-                    sb.AppendLine(member.Value.EmployeeId.ToString());
-                }
+        //public string GetMembers
+        //{
+        //    get
+        //    {
+        //        StringBuilder sb = new StringBuilder();
+        //        foreach (var member in _eventMembers)
+        //        {
+        //            sb.AppendLine(member.Value.EmployeeId.ToString());
+        //        }
 
-                return sb.ToString();
-            }
-        }
+        //        return sb.ToString();
+        //    }
+        //}
 
         public void AddMember(Employees employee)
         {
--- WorkPlannerSolution/WorkPlanner/ViewModel/LoginPageViewModel.cs
+++ WorkPlannerSolution/WorkPlanner/ViewModel/LoginPageViewModel.cs
@@ -4,7 +4,10 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 using WorkPlanner.Catalog;
+using WorkPlanner.Common;
+using WorkPlanner.Handler;
 using WorkPlanner.Model;
 
 namespace WorkPlanner.ViewModel
@@ -12,9 +15,14 @@ namespace WorkPlanner.ViewModel
     class LoginPageViewModel
     {
         private ObservableCollection<Employees> _employeeCatalog;
+        private Employees _selEmployees;
+        private string _password;
+        private Handler.LoginHandler _loginHander;
+        private ICommand _loginCommand;
 
         public LoginPageViewModel()
         {
+            _loginHander = new LoginHandler(this);
             _employeeCatalog = CatalogsSingleton.Instance.EmployeeCatalog.GetAll;
         }
 
@@ -25,5 +33,19 @@ namespace WorkPlanner.ViewModel
                 return _employeeCatalog;
             }
         }
+
+        public Employees SelEmployees
+        {
+            get { return _selEmployees; }
+            set { _selEmployees = value; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+        }
+
+
     }
 }
