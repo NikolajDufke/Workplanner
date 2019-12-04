520b38a3ab7269ee286da727373cddfae3444872
Benjamin Wieghorst Sørensen <benj448c@edu.easj.dk>
2019-12-04 11:00:17 +0100

    Loginhandler done
    loginpage done
    
    loginviewmodel done

--- WorkPlannerSolution/WorkPlanner/ViewModel/LoginPageViewModel.cs
+++ WorkPlannerSolution/WorkPlanner/ViewModel/LoginPageViewModel.cs
@@ -12,18 +12,18 @@ using WorkPlanner.Model;
 
 namespace WorkPlanner.ViewModel
 {
-    class LoginPageViewModel
+    class LoginPageViewModel : ViewModelBase
     {
         private ObservableCollection<Employees> _employeeCatalog;
         private Employees _selEmployees;
         private string _password;
         private Handler.LoginHandler _loginHander;
         private ICommand _loginCommand;
+        private string _message;
 
         public LoginPageViewModel()
         {
             _employeeCatalog = new ObservableCollection<Employees>(CatalogsSingleton.Instance.EmployeeCatalog.GetAll);
-         
             _loginHander = new LoginHandler(this);
          
         }
@@ -48,6 +48,25 @@ namespace WorkPlanner.ViewModel
             set { _password = value; }
         }
 
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand CreateEmployeeCommand
+        {
+            get
+            {
+                return _loginCommand ??
+                       (_loginCommand = new RelayCommand(_loginHander.LoginUser));
+            }
+            set { _loginCommand = value; }
+        }
 
     }
 }
