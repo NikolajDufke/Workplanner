0ab27efc38487b88134343eae237ff29fe9be48e
Nikolaj Dufke <nikolajdufke@live.dk>
2019-12-10 21:25:44 +0100

    DatetimeConverter til calender view. convertere så den kun viser dato.

---
+++ WorkPlannerSolution/WorkPlanner/Converter/DateFormatConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+
+namespace WorkPlanner.Converter
+{
+
+    public class DateFormatConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null)
+            {
+                DateTime dt = DateTime.Parse(value.ToString());
+                return dt.ToString("dd/MM");
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
